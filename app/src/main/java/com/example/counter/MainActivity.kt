2f32afce8593c7cee5d4361ba233226a696f5578
reachv <reachvann@gmail.com>
2023-10-04 15:53:17 -0700

    Intialization of Fragments and Bottom Navigation

--- app/src/main/java/com/example/counter/MainActivity.kt
+++ app/src/main/java/com/example/counter/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.counter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.counter.fragment.FavoritesFragment
+import com.example.counter.fragment.HomeFragment
+import com.example.counter.fragment.SettingsFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        //Declaration
+        var bottomnav : BottomNavigationView = findViewById(R.id.Botnav)
+        var fragmentManager = supportFragmentManager
+        var fragment = Fragment()
+        var homeFragment = HomeFragment()
+        var favoritesFragment = FavoritesFragment()
+        var settingFragment = SettingsFragment()
+
+        //Switch case to move Fragments
+        bottomnav.setOnItemSelectedListener {
+            if(it.itemId == R.id.home){
+                fragment = homeFragment
+            }else if(it.itemId == R.id.bfavor){
+                fragment = favoritesFragment
+            }else if(it.itemId == R.id.settings){
+                fragment = settingFragment
+            }
+            fragmentManager.beginTransaction().replace(R.id.container, fragment).commit()
+            return@setOnItemSelectedListener true
+        }
+        //Default Fragment Case
+        bottomnav.selectedItemId = R.id.home
     }
 }
--- app/src/main/java/com/example/counter/registerActivity.kt
+++ app/src/main/java/com/example/counter/registerActivity.kt
@@ -1,11 +1,41 @@
 package com.example.counter
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import com.parse.ParseUser
 
 class registerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        var register : Button = findViewById(R.id.register)
+        var user : EditText = findViewById(R.id.registerUser)
+        var pass : EditText = findViewById(R.id.registerPass)
+
+
+
+        register.setOnClickListener {
+            var user = ParseUser()
+            user.username = user.username.toString()
+            user.setPassword(pass.text.toString())
+
+            user.signUpInBackground {
+                if(it != null){
+                    Log.e("Register", "exception: " + it.toString())
+                    return@signUpInBackground
+                }
+                goMainActivity()
+            }
+        }
+    }
+
+    private fun goMainActivity() {
+        var intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
 }
