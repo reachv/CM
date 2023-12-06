b152f112fd0dd5f56f2f41716d8207be12daa32f
reachv <reachvann@gmail.com>
2023-12-06 11:09:26 -0800

    Finalv1

--- app/src/main/java/com/example/counter/activities/registerActivity.kt
+++ app/src/main/java/com/example/counter/activities/registerActivity.kt
@@ -7,7 +7,9 @@ import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import com.example.counter.R
+import com.parse.LogInCallback
 import com.parse.ParseUser
+import com.parse.SignUpCallback
 
 class registerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,23 +17,31 @@ class registerActivity : AppCompatActivity() {
         setContentView(R.layout.activity_register)
 
         var register : Button = findViewById(R.id.register)
-        var user : EditText = findViewById(R.id.registerUser)
+        var username : EditText = findViewById(R.id.registerUser)
         var pass : EditText = findViewById(R.id.registerPass)
 
 
 
         register.setOnClickListener {
-            var user = ParseUser()
-            user.username = user.username.toString()
+            val user  = ParseUser()
+            user.username = username.text.toString()
             user.setPassword(pass.text.toString())
 
-            user.signUpInBackground {
-                if(it != null){
-                    Log.e("Register", "exception: " + it.toString())
-                    return@signUpInBackground
+            user.signUpInBackground(SignUpCallback() {
+                if (it != null) {
+                    Log.e("Registery", "SignUpException: " + it)
                 }
-                goMainActivity()
-            }
+                ParseUser.logInInBackground(
+                    username.text.toString(),
+                    pass.text.toString(),
+                    LogInCallback { _, e ->
+                        if (e != null) {
+                            Log.e("logInInBackground", "Exception = " + e)
+                            return@LogInCallback
+                        }
+                        goMainActivity()
+                    })
+            })
         }
     }
 
